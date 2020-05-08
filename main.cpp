a199c5444e5c213139a22583b0e5889177a90e87
KevinCL94 <niveyl@hotmail.com>
2020-05-08 21:22:36 +0200

    Implementada funcionalidad de invertir el cohete con flag '-i', corrección caracter a utilizar para pie del cohete. A falta de repasar y comentar el código. A falta de compilar con CMake que presenta problemas con el compilador

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 #include <cctype>
 #include <cmath>
+#include <cstring>
 using namespace std;
- /**
-  * This is just a template
-  */
   
 bool isNum(const char *cadena)
 {
   for( ; *cadena; ++cadena )
   {
-    // En cuanto un caracter no sea numérico
     if( !isdigit(*cadena) )
       return false;
   }
@@ -24,13 +21,13 @@ void printSpaces(int numLines)
 		cout << " " ;
 	}
 }
-void printAsterisks(int numLines)
+void printCross(int numLines)
 {
 	for(int i = numLines; i < 2*numLines;i++){
-		cout << "*" ;
+		cout << "X" ;
 	}
 	for(int i = 2*numLines-2; i >= numLines;i--){
-		cout << "*" ;
+		cout << "X" ;
 	}
 }
 void printSpike(int numLines)
@@ -44,7 +41,7 @@ void printFeet(int numLines)
 	int finLinea = 2*numLines-1;
 	for(int i=0;i < finLinea; i++){
 		if(i == 0 || i == trunc(finLinea/2) || i == finLinea-1){
-			cout << "|";
+			cout << "X";
 		}else{
 			cout << " ";
 		}
@@ -55,10 +52,36 @@ void printFeet(int numLines)
 void printLine(int numLine, int numLines)
 {
 	printSpaces(numLines-numLine);
-	printAsterisks(numLine);
+	printCross(numLine);
 	cout << endl;
 }
 
+void printRocket(int numLines, bool inverse){
+	if(numLines){
+		if(!inverse){
+			for(int i = 0; i <= numLines+1; i++){
+				if(i == 0){
+					printSpike(numLines);
+				}else if(i <= numLines){
+					printLine(i, numLines);
+				}else{
+					printFeet(numLines);
+				}
+			}
+		}else{
+			for(int i = numLines+1; i >= 0; i--){
+				if(i == 0){
+					printSpike(numLines);
+				}else if(i <= numLines){
+					printLine(i, numLines);
+				}else{
+					
+					printFeet(numLines);
+				}
+			}
+		}
+	}
+}
 int main(int argc, char *argv[]) 
 {
 	bool inverse = false;
@@ -70,21 +93,26 @@ int main(int argc, char *argv[])
 	} else if (argc == 2) {
 		if(!isNum(argv[1])){
 			cout << "El primer parametro debe ser un numero" << endl;
+			return 0;
 		} else{
 			numLines = atoi(argv[1]);
 		}
-	} 
-	for(int i = 0; i <= numLines+1; i++){
-		if(i == 0){
-			printSpike(numLines);
-		}else if(i <= numLines){
-			printLine(i, numLines);
+	} else {
+		if(!isNum(argv[1])){
+			cout << "El primer parametro debe ser un numero" << endl;
+			return 0;
+		} else{
+			numLines = atoi(argv[1]);
+		}
+		if(!strcmp(argv[2],"-i")){
+			inverse = true;
 		}else{
-			printFeet(numLines);
+			cout << "Para invertir el cohete el tercer parametro debe ser '-i' " << endl;
 		}
 	}
+	printRocket(numLines,inverse);
 
-	
+	cout << endl << "by Kevin Caceres Luna" << endl;
 	cout << "Blau Ring Labs rules!" << endl;
 	
 	return 0;
