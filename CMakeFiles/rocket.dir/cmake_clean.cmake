02e3b2e2751965dd4df27f750d4a216e04623272
KevinCL94 <niveyl@hotmail.com>
2020-05-09 19:27:31 +0200

    Compilado y generado desde terminal, con >>cmake CMakeLists.txt && mingw32-make; aunque tomando fichero CMakeCache creado al compilar desde CMake-gui, sin este fichero previamente generado, la llamada da error al comprobar el funcionamiento del compilador

---
+++ CMakeFiles/rocket.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rocket.dir/main.obj"
+  "librocket.dll.a"
+  "rocket.exe"
+  "rocket.exe.manifest"
+  "rocket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rocket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
